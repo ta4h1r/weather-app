622c0d623ffb81d3577d1a58b0d1ca60ca86290d
Taahir Bhaiyat <taahir.bhaiyat@camcog.com>
2023-10-18 22:24:48 +0200

    Add auth middleware

---
+++ app/middleware/auth.py
@@ -0,0 +1,42 @@
+import base64
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+
+def is_authenticated(username, password):
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return True
+    else:
+        return False
+
+
+def auth_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        # Check auth
+        headers = request.headers
+        try:
+            if (headers['Authorization'].split(" ")[0] != "Basic"):
+                HttpResponse.status_code = 403
+                return HttpResponse("Auth not supported")
+            auth_data = base64.b64decode(headers['Authorization'].split(" ")[
+                                         1]).decode().split(":")
+            if (not is_authenticated(auth_data[0], auth_data[1])):
+                HttpResponse.status_code = 403
+                return HttpResponse("Unauthorized")
+        except (KeyError) as e:
+            return HttpResponse("Invalid auth header")
+
+        return response
+
+    return middleware
--- app/weathersite/settings.py
+++ app/weathersite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.auth.auth_middleware',
 ]
 
 ROOT_URLCONF = 'weathersite.urls'
