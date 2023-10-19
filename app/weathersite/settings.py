9ae3f85ce81b44f890a08acc4438630e2edae317
Taahir Bhaiyat <taahir.bhaiyat@camcog.com>
2023-10-19 22:59:29 +0200

    Cleanup unnecessary files

--- app/weathersite/settings.py
+++ app/weathersite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "weatherservice.apps.WeatherserviceConfig",
-    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- server/src/utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from constants import ERR_CODES
-
-def printTime():
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print("\n" + dt_string)
