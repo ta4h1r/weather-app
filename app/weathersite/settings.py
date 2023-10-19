da031cbacf9824f5bb676f5aaa26681cc01ccc37
Taahir Bhaiyat <taahir.bhaiyat@camcog.com>
2023-10-19 23:59:00 +0200

    Add build files

--- app/weathersite/settings.py
+++ app/weathersite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+a3fgkv4qkm2!c98ysivrgv&v6a-r&qvb(!7j#(lhp(#y7wp#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'www.jbgc.co.za' ]
 
 
 # Application definition
